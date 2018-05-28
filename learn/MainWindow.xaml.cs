0e3b2ad62c82303c811dc21a4320f2fb6a6fc530
kolik29 <30481034+kolik29@users.noreply.github.com>
2018-05-28 17:35:01 +0300

    больше баз данных

--- learn/MainWindow.xaml.cs
+++ learn/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SQLite;
 
 namespace learn
 {
@@ -27,9 +28,40 @@ namespace learn
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            adminWindow cw = new adminWindow();
-            cw.Show();
-            this.Close();
+            SQLiteConnection conn = new SQLiteConnection("Data Source=base.db; Version=3;");
+            try
+            {
+                conn.Open();
+                SQLiteCommand sql_command = conn.CreateCommand();
+                sql_command.CommandText = "SELECT grup FROM users WHERE login='" + login.Text + "' AND pass='" + pass.Text + "'";
+
+                SQLiteDataReader reader = sql_command.ExecuteReader();
+                reader.Read();
+
+                if (reader.StepCount == 0)
+                {
+                    MessageBox.Show("Авторизация не удалась.");
+                }
+                else
+                {
+                    if (reader[0].ToString() == "Администраторы")
+                    {
+                        adminWindow w = new adminWindow();
+                        w.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        ContentWindow w = new ContentWindow();
+                        w.Show();
+                        this.Close();
+                    }
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
--- learn/adminWindow.xaml.cs
+++ learn/adminWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using CefSharp.Wpf; //для работы с Chromium в wpf
+using System.Data.SQLite;
+using System.IO;
+using System.Collections.ObjectModel;
 
 namespace learn
 {
@@ -23,6 +26,12 @@ namespace learn
         public adminWindow()
         {
             InitializeComponent();
+        }
+
+        public class tree_node //объявление класса
+        {
+            public string Name { get; set; } //объявление параметров
+            public ObservableCollection<tree_node> Nodes { get; set; }
         }
 
         private void generate_pass_Click(object sender, RoutedEventArgs e)
@@ -41,6 +50,7 @@ namespace learn
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            ObservableCollection<tree_node> nodes = new ObservableCollection<tree_node>();
             chromiumWebBrowser = new ChromiumWebBrowser();
             chromiumWebBrowser.Address = Environment.CurrentDirectory +  @"\include\index.html";
             chromiumWebBrowser.Margin = new Thickness(5, 30, 5, 40);
@@ -49,6 +59,80 @@ namespace learn
 
             Grid.SetRow(chromiumWebBrowser, 1);
             browser_panel.Children.Add(chromiumWebBrowser);
+
+            SQLiteConnection conn = new SQLiteConnection("Data Source=base.db; Version=3;");
+            try
+            {
+                conn.Open();
+                SQLiteCommand sql_command = conn.CreateCommand();
+                sql_command.CommandText = "SELECT grup FROM users";
+
+                SQLiteDataReader reader = sql_command.ExecuteReader();
+                reader.Read();
+
+                List<string> groups = new List<string>();
+
+                while (reader.Read())
+                    groups.Add(reader[0].ToString());
+
+                groups = groups.Distinct().ToList();
+
+                foreach (string gr in groups)
+                    nodes.Add(new tree_node { Name = gr });
+
+                for (int i = 0; i < groups.Count; i++)
+                {
+                    sql_command.CommandText = "SELECT lName, fName, mName FROM users WHERE grup='" + groups[i].ToString() + "'";
+                    while (reader.Read())
+                    {
+                        nodes[i].Nodes.Add(new tree_node { Name = reader[0].ToString() });
+                    }
+                }
+            }
+
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            all_users.ItemsSource = nodes;
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=base.db; Version=3;");
+            try
+            {
+                conn.Open();
+                SQLiteCommand sql_command = conn.CreateCommand();
+                sql_command.CommandText = "SELECT * FROM users WHERE login='" + login.Text + "'";
+
+                SQLiteDataReader reader = sql_command.ExecuteReader();
+                reader.Read();
+
+                if (reader.StepCount == 0)
+                {
+                    reader.Close();
+                    sql_command = conn.CreateCommand();
+                    sql_command.CommandText = @"INSERT INTO users(lName, fName, mName, grup, login, pass) VALUES('" + 
+                                                lName.Text + "', '" + 
+                                                fName.Text + "', '" + 
+                                                mName.Text + "', '" + 
+                                                group.Text + "', '" +
+                                                login.Text + "', '" +
+                                                pass.Text + "')";
+
+                    sql_command.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь с таким логином уже существует.");
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
